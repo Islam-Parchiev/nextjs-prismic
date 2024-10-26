df81fd1b37d311a603a51ce9d7addd14cae8b94d
Islam Parchiev <islamparchev006@gmail.com>
2024-10-26 17:54:05 +0400

    feat: Add Homepage Page Title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
-// import Image from "next/image";
+import { Metadata } from "next";
+import { SliceZone } from "@prismicio/react";
 
-export default function Home() {
-  return (
-    <main className="">
+import { createClient } from "@/prismicio";
+// import { components } from "@/slices";
 
-      <h1 className="font-display text-5xl">I want you</h1>
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
 
-      {/* 1:16:00 */}
-    </main>
-  );
+
+  return <div className="text-red-700">Homepage!</div>
+
+  // return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
 }
